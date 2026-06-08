8fc04f90bd20dd82182a9fe608cf73fbafce6aac
BrenodePaiva <brenopaiva552@gmail.com>
2026-06-08 11:42:31 -0300

    feat: change skeleton animation to wave

--- src/components/SkeletonProject/index.jsx
+++ src/components/SkeletonProject/index.jsx
@@ -17,26 +17,26 @@ function SkeletonProject() {
             viewport={{ once: true, amount: 0.3 }}
         >
             <BoxProject>
-                <Skeleton variant="rectangular" width={300} height={191}/>
+                <Skeleton variant="rectangular" animation="wave" width={300} height={191}/>
                 <TextProjetc>
-                <Skeleton variant="text" width={130} sx={{fontSize: '28px'}} />
+                <Skeleton variant="text" animation="wave" width={130} sx={{fontSize: '28px'}} />
                 <div>
-                <Skeleton variant="text" width={280} sx={{fontSize: '16px'}} />
-                <Skeleton variant="text" width={280} sx={{fontSize: '16px'}} />
-                <Skeleton variant="text" width={280} sx={{fontSize: '16px'}} />
-                <Skeleton variant="text" width={280} sx={{fontSize: '16px'}} />
-                <Skeleton variant="text" width={280} sx={{fontSize: '16px'}} />
+                <Skeleton variant="text" animation="wave" width={280} sx={{fontSize: '16px'}} />
+                <Skeleton variant="text" animation="wave" width={280} sx={{fontSize: '16px'}} />
+                <Skeleton variant="text" animation="wave" width={280} sx={{fontSize: '16px'}} />
+                <Skeleton variant="text" animation="wave" width={280} sx={{fontSize: '16px'}} />
+                <Skeleton variant="text" animation="wave" width={280} sx={{fontSize: '16px'}} />
                 </div>
                 
                 <div style={{margin: '10px 0px'}}>
-                    <Skeleton variant="text" width={100} sx={{fontSize: '16px'}}/>
-                    <Skeleton variant="text" width={230} sx={{fontSize: '16px'}}/>
-                    <Skeleton variant="text" width={230} sx={{fontSize: '16px'}}/>
-                    <Skeleton variant="text" width={230} sx={{fontSize: '16px'}}/>
+                    <Skeleton variant="text" animation="wave" width={100} sx={{fontSize: '16px'}}/>
+                    <Skeleton variant="text" animation="wave" width={230} sx={{fontSize: '16px'}}/>
+                    <Skeleton variant="text" animation="wave" width={230} sx={{fontSize: '16px'}}/>
+                    <Skeleton variant="text" animation="wave" width={230} sx={{fontSize: '16px'}}/>
                 </div>
 
                 <ButtonContent>
-                <Skeleton variant="rounded" width={166} height={36}/>
+                <Skeleton variant="rounded" animation="wave" width={166} height={36}/>
                 </ButtonContent>
                 </TextProjetc>
             </BoxProject>
